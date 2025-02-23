0908f3f75c5a742adbb7aebdef594c6da3ddd7a9
Andres Muniz Piniella <amp-git@c4ad.eu>
2025-02-23 16:56:31 +0000

    Some instructions in the about page.

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -8,7 +8,20 @@ const About = () => {
       <p>☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️</p>
       <p>Trying to keep dry</p>
       <p>☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️☀️</p>
-      {/* <Droplets /> */}
+      <p>
+        This app will show you the humidity levels for today at noon. If at any
+        moment the temperature is bellow dhe dew point the plot will show red
+        rather than the usual light blue.{' '}
+      </p>
+      <p>
+        Fortnight shows the humidity values from last week and the prediction
+        for next week. You can click on any section of the plot and it will give
+        you some extra information for that time.{' '}
+      </p>
+      <p>
+        Fortnight Noon will give you the same information but will indicate the
+        day of the measurement.{' '}
+      </p>
     </article>
   )
 }
